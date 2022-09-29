c48fed4e776cdb1a91443c2308f79cdbdaab2e43
ldslucas23 <ldslucas2002@gmail.com>
2022-09-28 22:37:55 -0300

    Commit inicial

---
+++ src/main/java/com/findeyourbro/FindeyourbroApplication.java
@@ -0,0 +1,13 @@
+package com.findeyourbro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindeyourbroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindeyourbroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findeyourbro/FindeyourbroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findeyourbro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindeyourbroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
