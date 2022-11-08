dbf9176e2b5dee5ac6c00f4a09ae5270ac50c5e1
ldslucas23 <ldslucas2002@gmail.com>
2022-11-07 21:35:19 -0300

    Adicionada geração de ids String

--- src/main/java/com/findeyourbro/service/chat/ChatRoomService.java
+++ src/main/java/com/findeyourbro/service/chat/ChatRoomService.java
@@ -1,6 +1,7 @@
 package com.findeyourbro.service.chat;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -30,11 +31,13 @@ public class ChatRoomService {
                             String.format("%s_%s", senderId, recipientId);
 
                     ChatRoom senderRecipient = new ChatRoom();
+                    senderRecipient.setId(UUID.randomUUID().toString());
                     senderRecipient.setSenderId(senderId);
                     senderRecipient.setChatId(chatId);
                     senderRecipient.setRecipientId(recipientId);
 
                     ChatRoom recipientSender = new ChatRoom();
+                    recipientSender.setId(UUID.randomUUID().toString());
                     recipientSender.setChatId(chatId);
                     recipientSender.setSenderId(recipientId);
                     recipientSender.setRecipientId(senderId);
