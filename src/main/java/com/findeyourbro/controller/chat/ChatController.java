b02a2786098454b65762bb6348f9612ef11f09dd
ldslucas23 <ldslucas2002@gmail.com>
2022-11-07 22:35:50 -0300

    Adicionados endpoints de listagem de mensagens já recebidas

--- src/main/java/com/findeyourbro/controller/chat/ChatController.java
+++ src/main/java/com/findeyourbro/controller/chat/ChatController.java
@@ -1,10 +1,13 @@
 package com.findeyourbro.controller.chat;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.findeyourbro.model.chat.ChatMessage;
 import com.findeyourbro.model.chat.ChatNotification;
@@ -42,6 +45,28 @@ public class ChatController {
         messagingTemplate.convertAndSendToUser(
                 String.valueOf(chatMessage.getRecipientId()),"/queue/messages",
                 chatNotification);
+    }
+    
+    @GetMapping("/messages/{senderId}/{recipientId}/count")
+    public ResponseEntity<Long> countNewMessages(
+            @PathVariable Long senderId,
+            @PathVariable Long recipientId) {
+
+        return ResponseEntity
+                .ok(chatMessageService.countNewMessages(senderId, recipientId));
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<?> findChatMessages(@PathVariable Long senderId,
+                                              @PathVariable Long recipientId) {
+        return ResponseEntity
+                .ok(chatMessageService.findChatMessages(senderId, recipientId));
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<?> findMessage (@PathVariable Long id) {
+        return ResponseEntity
+                .ok(chatMessageService.findById(id));
     }
 }
 
